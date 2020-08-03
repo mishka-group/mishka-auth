6eff795ac33f86c833bebea3ce23c608730b7c8d
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2020-08-03 21:45:40 +0430

    chenge gurdian project name

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule MishkaAuth.MixProject do
       version: "0.0.1",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
-      # config_path: "./config/config.exs",
+      config_path: "./config/config.exs",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env() == :prod,
       description: description(),
